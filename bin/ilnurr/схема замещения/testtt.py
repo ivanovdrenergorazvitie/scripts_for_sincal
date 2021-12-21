baf4371e20b5fad9017007f776478ec5d2094baf
student22 <student22@er.int>
2021-12-21 13:02:51 +0300

    21.12.21

--- bin/ilnurr/схема замещения/testtt.py
+++ bin/ilnurr/схема замещения/testtt.py
@@ -1,15 +1,10 @@
-uwu = []
-def h():
-    uwu.append('s')
-    print(12)
-    uwu.append('s')
-def f():
-  g(h)
-  uwu.append('s')
-def g(a):
-  a()
-  uwu.append('s')
-uwu.append('s')
-g(f)
+a = 0
+def foo():
+  b = 1
+  get(a)
+  get(c)
+  def bar():
+    a = 2
+    get(a)
+    get(b)
 
-print(uwu)
